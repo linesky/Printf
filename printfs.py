afb03a3198ad24824f5a6e3b00427855180e66cc
sky line <32512284+linesky@users.noreply.github.com>
2024-02-20 18:54:44 +0000

    Add files via upload

--- printfs.py
+++ printfs.py
@@ -1,11 +1,22 @@
-def printfs(*vars):
+def printfs(*varss:list):
+    
+    
+    vars:list=[]
+    for n in varss:
+        vars=vars+[n]
+    
+  
     lens=len(vars)
+    
+    
     
     i:int=0
     if lens==0:
-        i=0
+        ii=0
+
     if lens==1:
         print(vars[0])
+
     else:
         spl=vars[0].split("%")
         lens2=len(spl)
@@ -17,9 +28,13 @@ def printfs(*vars):
                 if len(ss)>1:
                     
                     print(ss[1:],end="")
-            if n<lens-1:
-                ss=vars[n-1]
-                print(ss)
-         
 
-printfs("\x1bc\x1b[41;37mhello world %s  \n",10)
+ 
+            if n<lens-1:
+                ss=vars[n+1]
+                print(ss,end="")
+           
+         
+print("\x1bc\x1b[41;37m")
+for nn in range(10):
+    printfs("hello world %s %s \n",nn,"#####")
