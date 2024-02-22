2de20f2082b28b5b35b6fb4d2fc42fe7ac3c9641
sky line <32512284+linesky@users.noreply.github.com>
2024-02-22 17:33:44 +0000

    Add files via upload

---
+++ space.py
@@ -0,0 +1,52 @@
+def spacecodes(*varss):
+    svars=list(*varss)
+    
+    sreturn:str=""
+    vars:list=[]
+    for n in svars:
+        vars=vars+[str(n)]
+    
+  
+    lens=len(vars)
+    
+    
+    
+    i:int=0
+    if lens==0:
+        ii=0
+
+    if 0==0:
+       
+        spl=vars[0].split("<")
+        
+        lens2=len(spl)
+        for n in range(0,lens2):
+            ss=spl[n]
+            if n==0:
+                sreturn=sreturn+str(ss)
+           
+            else:
+                if 0==0:
+                    kkk=ss.split(">")
+                    
+                    llll=" "*int(kkk[0])
+                    
+                    if len(kkk)>1:
+                        sreturn=sreturn+str(llll)+str(kkk[1])
+                   
+ 
+           
+    return sreturn
+
+
+def printspacecodes(*varss):
+    vars:list=[]
+    for n in varss:
+        vars=vars+[str(n)]
+        
+    print(spacecodes(vars))        
+         
+print("\x1bc\x1b[41;37m")
+
+for nn in range(10):
+    printspacecodes("hello world<033>XXX<034>xxx<010>mmmm")
