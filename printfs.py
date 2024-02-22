b466a88028dc4ff220b0592c451a6873e9dcaaf6
sky line <32512284+linesky@users.noreply.github.com>
2024-02-22 07:35:46 +0000

    Add files via upload

--- printfs.py
+++ printfs.py
@@ -1,9 +1,10 @@
-def printfs(*varss:list):
+def sprintfs(*varss):
+    svars=list(*varss)
     
-    
+    sreturn:str=""
     vars:list=[]
-    for n in varss:
-        vars=vars+[n]
+    for n in svars:
+        vars=vars+[str(n)]
     
   
     lens=len(vars)
@@ -15,25 +16,32 @@ def printfs(*varss:list):
         ii=0
 
     if lens==1:
-        print(vars[0])
+        sreturn=sreturn+str(vars[0])
 
     else:
         spl=vars[0].split("%")
+        
         lens2=len(spl)
         for n in range(0,lens2):
             ss=spl[n]
             if n==0:
-                print(ss,end="")
+                sreturn=sreturn+str(ss)
             else:
                 if len(ss)>1:
                     
-                    print(ss[1:],end="")
+                    sreturn=sreturn+str(ss[1:])
 
  
             if n<lens-1:
                 ss=vars[n+1]
-                print(ss,end="")
-           
+                sreturn=sreturn+str(ss)
+    return sreturn
+def printfs(*varss):
+    vars:list=[]
+    for n in varss:
+        vars=vars+[str(n)]
+        
+    print(sprintfs(vars))        
          
 print("\x1bc\x1b[41;37m")
 for nn in range(10):
