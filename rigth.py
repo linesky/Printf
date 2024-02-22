6d40d9bb84e7f130d91f6ec48877cc693df44b36
sky line <32512284+linesky@users.noreply.github.com>
2024-02-22 19:56:13 +0000

    Add files via upload

---
+++ rigth.py
@@ -0,0 +1,65 @@
+def stringcodes(*varss):
+    svars=list(*varss)
+    
+    sreturn:str=""
+    vars:list=[]
+    for n in svars:
+        vars=vars+[str(n)]
+    
+  
+    lens=len(vars)
+    
+    
+    
+    i:int=0
+    if lens==0:
+        ii=0
+
+    if 0==0:
+       
+        spl=vars[0].split("«")
+        
+        lens2=len(spl)
+        for n in range(0,lens2):
+            ss=spl[n]
+            if n==0:
+                sreturn=sreturn+str(ss)
+           
+            else:
+                if 0==0:
+                    kkk=ss.split("»")
+                    
+                    
+                    
+                    if len(kkk)>1:
+                        
+                        jjj=kkk[0].split(";")
+                        
+                        if len(jjj)>1:
+                            l1:str=jjj[0]
+                            l2:int=int(jjj[1])
+                            llll:str=l2*l1
+                            sreturn=sreturn+str(llll)+str(kkk[1])
+                   
+ 
+           
+    return sreturn
+
+
+def printstringcodes(*varss):
+    vars:list=[]
+    for n in varss:
+        vars=vars+[str(n)]
+        
+    print(stringcodes(vars))        
+         
+print("\x1bc\x1b[41;37m")
+n=1
+justifys=25
+for nn in range(10):
+    sn=str(n)
+    snl=len(sn)
+    f=justifys-snl
+    ttt="« ;"+str(f)+"»"+sn
+    printstringcodes(ttt)
+    n*=10
