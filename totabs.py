a07cc759c0e0e06bd0a8fbcaf77e657b6e61bfcd
sky line <32512284+linesky@users.noreply.github.com>
2024-02-26 07:34:13 +0000

    Add files via upload

---
+++ totabs.py
@@ -0,0 +1,11 @@
+def totabs(s:str,i:int):
+    xx:int=i
+    xxx:str=" "
+    xxx=xxx*xx
+    
+    xxx=xxx+s
+    
+    print(xxx)
+print("\x1bc\x1b[41;37m")
+for i in range(10):
+    totabs("*"*(i*2),80)
